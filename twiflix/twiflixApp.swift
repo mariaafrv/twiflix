5b56f120a3d4834e05baaf243be37189b532afa1
Maria Aida Fernandes Vitoria <mariaafv@hotmail.com>
2025-04-05 11:29:04 -0300

    Initial Commit

---
+++ twiflix/twiflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  twiflixApp.swift
+//  twiflix
+//
+//  Created by Maria Aida Fernandes Vitoria - Unicred do Brasil on 05/04/25.
+//
+
+import SwiftUI
+
+@main
+struct twiflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
